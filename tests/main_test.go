31eca9455a99d331850b23c7c1ac91f2f791f8e5
Morior <moriorgames@gmail.com>
2018-07-31 19:37:52 +0200

    Experimenting splitting packages into different directories

--- tests/main_test.go
+++ tests/main_test.go
@@ -5,19 +5,14 @@ import (
 	"github.com/moriorgames/agent-smith/lib"
 )
 
-
-// 	"github.com/drone/drone/version"
-// https://github.com/drone/drone/version
-// https://github.com/moriorgames/agent-smith/smith
-
 func TestSum(t *testing.T) {
-	if smith.add(2, 5) != 7 {
+	if smith.Add(2, 5) != 7 {
 		t.Fail()
 	}
-	if add(2, 100) != 102 {
+	if smith.Add(2, 100) != 102 {
 		t.Fail()
 	}
-	if add(222, 100) != 322 {
+	if smith.Add(222, 100) != 322 {
 		t.Fail()
 	}
 }
