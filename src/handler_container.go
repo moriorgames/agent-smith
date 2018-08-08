64175fc6d44b06649820b71ffcb3e9e349dfb4e0
Morior <moriorgames@gmail.com>
2018-08-08 18:13:09 +0200

    Naming routes

--- src/handler_container.go
+++ src/handler_container.go
@@ -20,3 +20,12 @@ func ViewContainer(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	fmt.Fprintf(w, renderContainerContent(id))
 }
+
+func UpdateContainer(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintf(w, renderContainerContent(id))
+
+	// Save container
+
+	//http.Redirect(w, r, newUrl, http.StatusSeeOther)
+}
--- src/router.go
+++ src/router.go
@@ -4,12 +4,20 @@ import "github.com/gorilla/mux"
 
 func CreateRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", ViewHome)
-	router.HandleFunc("/container", ViewContainer)
-	router.HandleFunc("/container/{id}", ViewContainer)
-	router.HandleFunc("/static/css/{file}", StaticCss)
-	router.HandleFunc("/static/js/{file}", StaticJs)
-	router.HandleFunc("/static/img/{file}", StaticImg)
+	router.HandleFunc("/", ViewHome).
+		Methods("GET").Name("app_home")
+	router.HandleFunc("/container", ViewContainer).
+		Methods("GET").Name("app_new_container")
+	router.HandleFunc("/container/{id}", ViewContainer).
+		Methods("GET").Name("app_edit_container")
+	router.HandleFunc("/container/{id}", UpdateContainer).
+		Methods("POST").Name("app_update_container")
+	router.HandleFunc("/static/css/{file}", StaticCss).
+		Methods("GET")
+	router.HandleFunc("/static/js/{file}", StaticJs).
+		Methods("GET")
+	router.HandleFunc("/static/img/{file}", StaticImg).
+		Methods("GET")
 
 	return router
 }
