1cb948712e5ba333f692baf0d4815363cac93831
Morior <moriorgames@gmail.com>
2018-08-09 17:09:04 +0200

    Add more testing and js and css in absolute route

--- src/handler_container_test.go
+++ src/handler_container_test.go
@@ -7,6 +7,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIsAbleToFindNewContainerRoute(t *testing.T) {
+	req, err := http.NewRequest("GET", "/container", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router := CreateRouter()
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK)
+}
+
+func TestIsAbleToFindEditContainerRoute(t *testing.T) {
+	req, err := http.NewRequest("GET", "/container/1234", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router := CreateRouter()
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK)
+}
+
 func TestUpdateContainerRedirectsToEditView(t *testing.T) {
 	req, err := http.NewRequest("POST", "/container/1234", nil)
 	if err != nil {
